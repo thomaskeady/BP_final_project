fecb2b9f91f41d1cfb39a8fb6535ae0efaa40b56
thomaskeady <thomas.keady@jhu.edu>
2017-05-09 02:27:49 -0400

    Good progress

--- dispProps.m
+++ dispProps.m
@@ -6,7 +6,8 @@ image_ext = 'png';
 
 I = imread(strcat('images/', image_src, '/', image_name, '.', image_ext));
 
-stats = regionprops('table', I, 'Area', 'PixelIdxList');
+%stats = regionprops('table', I, 'Area', 'PixelIdxList');
+stats = regionprops('table', I, 'Area', 'PixelList');
 
 disp(stats);
 
@@ -14,6 +15,7 @@ max = 0;
 
 % Premature optimization is the root of all evil - 2:06AM, 5/9/17, t-minus
 % 10 hours to presentation
+% 
 
 for i = 1:height(stats)
     if stats{i, 1} > max
@@ -21,21 +23,40 @@ for i = 1:height(stats)
     end 
 end
 
-y_length = size(I, 2);   % good
-
 for i = 1:height(stats)
     if stats{i, 1} ~= max
         p = stats{i, 2};
         list = p{1}';
         
         for pix = list
-            y = floor(pix / y_length) + 1;
-            
-            x = mod(pix, y_length);
-            
-            I(x, y)
+            disp(I(pix));
+            temp = I(pix);
+            disp(I(pix(2), pix(1)));
+            temp2 = I(pix(2), pix(1));
+            disp('next');
             
         end
     end
-end            
+end 
+
+
+% 
+% x_length = size(I, 2);   % good
+% 
+% for i = 1:height(stats)
+%     if stats{i, 1} ~= max
+%         p = stats{i, 2};
+%         list = p{1}';
+%         
+%         for pix = list
+%             y = floor(pix / x_length) + 1;
+%             
+%             x = mod(pix, size(I, 1));
+%             
+%             I(x, y)
+%             
+%         end
+%     end
+% end            
+
 %imshow(I);
